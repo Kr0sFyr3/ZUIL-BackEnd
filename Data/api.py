f4c1aa1a638970c8e7d2d044828d167dde506a58
TimBanh <T_Tim@live.nl>
2023-11-22 13:09:23 +0100

    api-connector 1.0
    Added api class with login an get students function

---
+++ Data/api.py
@@ -0,0 +1,41 @@
+import requests
+
+
+class API:
+    def __init__(self):
+        self.base_url = "https://hub.adainforma.tk/api/v1"
+        self.header = {'Accept': 'application/json'}
+        self.token = None
+
+    def login(self, email, password):
+        login_url = f"{self.base_url}/login"
+        data = {'email': email, 'password': password}
+
+        try:
+            response = requests.post(login_url, headers=self.header, data=data)
+
+            authorisation = response.json().get('authorisation')
+            self.token = authorisation.get('token')
+            token_type = authorisation.get('type')
+
+            if response.status_code == 200:
+                # Print the authentication token or handle it as needed
+                print(f"Login successful. Authentication Token: {self.token}")
+                self.header.update({'Authorization': f"Bearer {self.token}"})
+            else:
+                # Print an error message if the login was unsuccessful
+                print("Login failed. Status Code:", response.status_code, "Message:", response.text)
+
+        except requests.exceptions.RequestException as e:
+            # Handle exceptions, such as network errors
+            print("Error:", e)
+
+    def get_students(self):
+        students_url = f"{self.base_url}/student"
+
+        try:
+            response = requests.post(students_url, headers=self.header)
+
+            print(response.json())
+        except requests.exceptions.RequestException as e:
+            print("Error", e)
--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("Hello World")
+from Data.api import API
+
+api = API()
+
+api.login("tnc.banh1@ad-academie.nl", "20qKy_u}5R")
+
+api.get_students()
